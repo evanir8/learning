0e83e694586d9401cbdffa14a7fee25d0382ac84
Evanir de Oliveira Junior <evanir@evol.inf.br>
2020-01-20 15:12:11 -0300

    from mac

---
+++ vue/alurapic.old/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+   <h1>{{ titulo }}</h1>
+  <ul>
+  <li v-for="foto of fotos">
+    <img :src="foto.url" :alt="foto.titulo">
+  </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+      {
+        url: 'https://img.ibxk.com.br/2019/07/26/26000559344397.jpg?w=1120&h=420&mode=crop&scale=both',
+        titulo: 'Eclipse'
+      },
+      {
+        url: 'https://imagens.canaltech.com.br/236607.471195-Lua.jpg',
+        titulo: 'Lua'
+      }
+      ]
+    }
+  }
+  //created(){
+  }
+}
+
+</script>
+
+<style>
+
+</style>
