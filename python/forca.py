208d83f0777716620062165551703b42d6ee5205
Evanir de Oliveira Junior <evanir@evol.inf.br>
2020-01-30 09:47:02 -0300

    implementando forca, upper find split

--- python/forca.py
+++ python/forca.py
@@ -1,14 +1,26 @@
 
 import random
 
+
 def jogar():
-  print("**********************************")
-  print("Bem vindo ao jogo de  Adivinhação!")
-  print("**********************************")
+    print("**********************************")
+    print("Bem vindo ao jogo de  Adivinhação!")
+    print("**********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ").strip().upper()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra.upper()):
+                print("Encontrado {} na posicao {}".format(letra, index))
+                index += 1
+        palavra_secreta.find(chute)
+        print("Jogando ...")
+    print("Fim do Jogo")
 
-  print("Fim do Jogo")
 
 if(__name__ == '__main__'):
     jogar()
-
