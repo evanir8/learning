0b3b6fd4c7c421e6cebac84e9aea165dd82bb07f
Evanir de Oliveira Junior <evanir@evol.inf.br>
2020-02-14 06:34:16 -0300

    criando classe e inicializando objetos

---
+++ python/oo/conta.py
@@ -0,0 +1,9 @@
+class Conta:
+
+  def__init__(self, numero, titular, saldo, limite):
+    print("Construindo o objeto ...")
+    self.numero  = numero
+    self.titular = titular
+    self.saldo   = saldo
+    self.limite  = limite
+
