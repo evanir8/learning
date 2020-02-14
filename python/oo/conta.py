ac30b1b4f2feb813a9fe97737cf4c678128a1b64
Evanir de Oliveira Junior <evanir@evol.inf.br>
2020-02-14 09:33:47 -0300

    continuando

--- python/oo/conta.py
+++ python/oo/conta.py
@@ -1,9 +1,19 @@
 class Conta:
 
-  def__init__(self, numero, titular, saldo, limite):
-    print("Construindo o objeto ...")
-    self.numero  = numero
-    self.titular = titular
-    self.saldo   = saldo
-    self.limite  = limite
+  pass
 
+  def __init__(self, numero, titular, saldo, limite):
+    print("Construindo o objeto ...{}".format(self))
+    self.numero = numero
+    self.titular = titular
+    self.saldo = saldo
+    self.limite = limite
+
+  def extrato(self):
+    print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
+  def deposita(self, valor):
+    self.saldo += valor
+
+  def saca(self, valor):
+    self.saldo -= valor
