9244eab13f6462f1d2143a6f40f9f9a448b340dc
Evanir de Oliveira Junior <evanir@evol.inf.br>
2020-02-17 06:21:38 -0300

    getter and setters

--- python/oo/conta.py
+++ python/oo/conta.py
@@ -21,3 +21,17 @@ class Conta:
   def transfere(self, valor, conta_destino):
     self.saca(valor)
     conta_Destino.deposita(valor)
+
+  def get_saldo(self)
+    return self.__saldo
+
+  def get_titular(self)
+    return self.__titular
+
+  @property
+  def limite(self)
+    return self.__limite
+
+  @limite.setter
+  def limite(self, novo_limite)
+    self.__limite = novo_limite
