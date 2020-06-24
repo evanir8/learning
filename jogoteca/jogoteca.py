1f48ab0c8311890003fafc77f6a8ec4512417192
Evanir <evanir@evol.inf.br>
2020-06-23 23:05:25 -0300

    aula 4

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,8 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-   return render_template('lista.html')
+class Jogo:
+   def __init__(self, nome, categoria, console):
+      self.nome = nome
+      self.categoria = categoria
+      self.console = console
 
-app.run()
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon', 'RPG', 'GBA')
+jogo3 = Jogo('Pinball', 'Arcade', 'PC')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/')
+def index():
+   return render_template('lista.html',
+                           titulo="Jogos",
+                           jogos=lista)
+
+@app.route('/novo')
+def novo():
+   return render_template('novo.html', titulo ='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    # return render_template('lista.html', titulo="Jogos", jogos=lista)
+    return redirect('/')
+
+
+
+
+app.run(debug=True)
